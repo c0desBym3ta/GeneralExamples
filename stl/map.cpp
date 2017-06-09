beb620ffab7de903756eaabbf85633ba3b5d1f32
c0desBym3ta <m3tahckr@hotmail.com>
2017-06-09 23:47:27 +0000

    Initializing the map stl.

---
+++ stl/map.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+using namespace std;
+
+void line();
+void lines();
+
+int main(){
+	cout << "Tesk, OK!" << endl;
+
+
+
+	return 0;
+}
+
+void line(){
+	cout << endl;
+}
+
+void lines(){
+	lines(); lines();
+}
+
