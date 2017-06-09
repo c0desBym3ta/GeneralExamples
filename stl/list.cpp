48eebe45aebad5c0d6c65765d1d5bcb072eae667
c0desBym3ta <m3tahckr@hotmail.com>
2017-06-09 22:42:15 +0000

    Initializing the list source code.

---
+++ stl/list.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+void line();
+void lines();
+
+int main(){
+	cout << "Test, OK!" << endl;
+		
+
+
+	return 0;
+}
+
+
+void line(){
+	cout << endl;
+}
+
+void lines(){
+	line();line();
+}
