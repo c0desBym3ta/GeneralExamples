34be9b47c726fde5466a88ade56c65d9d781605d
c0desBym3ta <kalimetasploiter@gmail.com>
2017-06-22 00:48:44 +0200

    Catch all the exception example and why.

--- Exceptions/multipleCatchBlock.cpp
+++ Exceptions/multipleCatchBlock.cpp
@@ -11,17 +11,13 @@ int main(){
 		//throw "Some exception."; //Will be called the first catch block
 		//throw 20; //Will be called the second int catch block.
 		throw runtime_error("Runtime error."); //The runtime error third catch will be called.
-	}catch(const char *err){
-		cout << err << endl;
+	
+	//We can write it at the end. If any other exception won't called then this general exception will.
+	//So we can have all our cathes. And if any catch won't work this instead will.
+	}catch(...){ //In this way we can catch all the exceptions we need.
+		cout << "Some excepttion occured." << endl;
 	}
-	catch(int error){
-		cout << error << endl;
-	}
-	catch(runtime_error &e){
-		cout << e.what() << endl;
-	}
-
-
+	
 
 	return 0;
 }
